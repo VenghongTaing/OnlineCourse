e26cbd99e66faabfdd65404d4c08e19d5005c8f5
Mon Mat <monsokmeanmon@gmail.com>
2022-10-12 21:41:59 +0700

    Add loginform

--- app/src/main/java/com/example/onlinecourse/LoginForm.java
+++ app/src/main/java/com/example/onlinecourse/LoginForm.java
@@ -5,16 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginForm extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Intent show_loginform = new Intent(MainActivity.this, LoginForm.class);
-        startActivity(show_loginform);
+        setContentView(R.layout.activity_login_form);
 
     }
-
-
 }
