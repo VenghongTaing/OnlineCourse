d5b9762dff00460ea97fa184fbfb2a4ea1c6d64f
VenghongTaing <j.ran1997@gmail.com>
2022-10-19 00:23:34 +0700

    New update on Dashboard activity and plugin library with material.io

--- app/src/main/java/com/example/onlinecourse/MainActivity.java
+++ app/src/main/java/com/example/onlinecourse/MainActivity.java
@@ -11,9 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent show_loginform = new Intent(MainActivity.this, LoginForm.class);
-        Intent show_signupform = new  Intent(MainActivity.this, Signup.class);
-        startActivity(show_signupform);
+//        Intent show_loginform = new Intent(MainActivity.this, LoginForm.class);
+//        Intent show_signupform = new  Intent(MainActivity.this, Signup.class);
+//        startActivity(show_signupform);
+
+        //start activity with dashboard
+        Intent show_dashboard = new Intent(MainActivity.this, Dashboard.class);
+        startActivity(show_dashboard);
 
     }
 
